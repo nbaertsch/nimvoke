f0439f8119d53dc23021e278d7d899dd659d692e
Noah Baertsch <16431857+nbaertsch@users.noreply.github.com>
2024-10-26 11:21:08 -0400

    Update common.nim

--- src/nimvoke/inc/common.nim
+++ src/nimvoke/inc/common.nim
@@ -53,17 +53,17 @@ when defined amd64:
 
 when defined i386:
     {.passC:"-masm=intel".}
-proc GetPEB*(): ptr PVOID {.asmNoStackFrame.} =
-    ## Uses inline assembly to get a pointer to the PEB
-    asm """
-        push rbx
-        xor rdi, rdi
-        mul rdi
-        mov rbx, rax
-        add rdi, 0x20
-        mov eax, fs:[rdi+0x10]
-        pop rbx
-        ret
-    """
+    proc GetPEB*(): ptr PVOID {.asmNoStackFrame.} =
+        ## Uses inline assembly to get a pointer to the PEB
+        asm """
+            push rbx
+            xor rdi, rdi
+            mul rdi
+            mov rbx, rax
+            add rdi, 0x20
+            mov eax, fs:[rdi+0x10]
+            pop rbx
+            ret
+        """
 
 pPEB = GetPEB()
